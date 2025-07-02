50092c345a833d8a06a43a27ecde5a1d6bc17fc4
kumar ankesh <kumarankesh1894@gmail.com>
2025-07-02 12:45:14 +0530

    Added CORS support in backend

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ import GenerateImageRouter from "./routes/GenerateImage.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://mari-ai.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
